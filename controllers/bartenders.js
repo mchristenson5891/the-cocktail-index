f6d9e5c2d96c4ce325c845371b358638ec0ffabc
Rebeca Levy <beckalevy@Rebecas-MacBook-Pro.local>
2019-10-28 18:58:09 -0700

    fix register route

--- controllers/bartenders.js
+++ controllers/bartenders.js
@@ -4,31 +4,34 @@ const Bartender = require('../models/bartenders');
 const Recipe = require('../models/recipes');
 const bcrypt = require('bcryptjs')
 
-router.post('/register', async (req, res) => {
 
-  try{
-    const foundBartender = await Bartender.find({username: req.body.username})
-    if(foundBartender){
-      res.send('Username already exists')
 
-    }else{
-      const password = req.body.password; // the password from the form
-      const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-      req.body.password = passwordHash
+// router.post('/register', async (req, res)=>{
+
+//   try{
+//     const foundBartender = await Bartender.find({username: req.body.username})
+//     if(foundBartender){
+//       res.send('Username already taken')
+//     }else{
+//       const password = req.body.password;
+//       const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
       
-      const createdBartender = await Bartender.create(req.body);
-      console.log(createdBartender)
-      req.session.username = createdBartender.username;
-      req.session.logged = true;
-    
-      res.redirect('/bartenders')
-    }
+//       const userDBEntry ={};
+//       userDBEntry.username = req.body.username;
+//       userDBEntry.password = passwordHash;
+//       userDBEntry.email    = req.body.email;
 
-  }catch(err){
-    res.send(err)
-  }
+//       const createdBartender= await Bartender.create(userDBEntry);
+//       console.log(createdBartender);
+//       req.session.username = createdBartender.username;
+//       req.session.logged = true;
+//       res.redirect('/bartenders')
+//     }
 
-});
+//   }catch(err){
+
+//   }
+// })
 
 router.post('/login', async(req, res)=>{
 
@@ -89,15 +92,58 @@ router.get('/new', (req, res )=>{
 })
 
 //create user profile
-router.post('/', async (req, res)=>{
+// router.post('/', async (req, res)=>{
 
-    try{
-        const createdBartender = await Bartender.create(req.body);
-        console.log(createdBartender)
-        res.redirect('/bartenders')
-    }catch(err){
-        res.send(err)
-    }
+//     try{
+//         const createdBartender = await Bartender.create(req.body);
+//         console.log(createdBartender)
+//         res.redirect('/bartenders')
+//     }catch(err){
+//         res.send(err)
+//     }
+// })
+
+//show route
+
+router.get('/:id', (req, res)=>{
+
+  try{
+    const foundBartender = Bartender.findById(req.params.id)
+    res.render('bartenders/show.ejs',{
+      bartender: foundBartender
+    })
+
+  }catch(err){
+    res.send(err)
+  }
 })
+
+router.post('/register', async (req, res) => {
+
+  try{
+    const foundBartender = await Bartender.findOne({username: req.body.username})
+    console.log(foundBartender, 'this is bartender')
+    if(foundBartender){
+      res.send('Username already exists')
+    }
+    else{
+      const password = req.body.password;
+      const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+      req.body.password = passwordHash
+      
+      const createdBartender = await Bartender.create(req.body);
+      console.log(createdBartender)
+      req.session.username = createdBartender.username;
+      req.session.logged = true;
+    
+      res.redirect('/bartenders')
+    }
+
+  }catch(err){
+    res.send(err)
+  }
+
+});
+
 
 module.exports = router;
